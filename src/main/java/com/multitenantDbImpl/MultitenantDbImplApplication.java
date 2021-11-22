e4984ad705a1e98cec409fbb83984edcf79e8458
sharmanikhil1100 <sharmanikhil1100@gmail.com>
2021-11-22 20:55:37 +0530

    project setup

---
+++ src/main/java/com/multitenantDbImpl/MultitenantDbImplApplication.java
@@ -0,0 +1,13 @@
+package com.multitenantDbImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantDbImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantDbImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multitenantDbImpl/MultitenantDbImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitenantDbImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantDbImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
